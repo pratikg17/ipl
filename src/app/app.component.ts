8524fb765347ae4661f81e7403189dc94477b4c8
Pratik <pjgawand@gmail.com>
2017-04-15 13:02:31 +0530

    Part 3.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 
+
+
+
+export class Team {
+  position: number;
+  name: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
-  title = 'app works!';
+  title = 'Welcome to IPL 2017';
+  
+  team: Team = {
+    position: 1,
+    name:"Mumbai Indian"
+  }
+
+
 }
